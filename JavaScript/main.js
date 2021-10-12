cf018609ab62315bb842f77102ee047998926228
Prakhar Sharma <56041320+Prakharsharma2206@users.noreply.github.com>
2021-10-12 23:53:58 +0530

    Up1

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -3,3 +3,18 @@ window.onload = function(){
       document.getElementById("loader").remove();
     },1000);
   };
+
+
+
+
+VANTA.NET({
+  el: "section",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  backgroundColor: 0x0
+})
