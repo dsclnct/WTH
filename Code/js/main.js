3e6db09aa93fdbdb5f1d91890c4c9eaa44198bbf
weebo-ary <aryan.bhardwaj1233@gmail.com>
2021-10-25 18:44:20 +0530

    updatedNavBtn

--- Code/js/main.js
+++ Code/js/main.js
@@ -1,3 +1,8 @@
+$(document).ready(function(){
+	$('#nav-icon2').click(function(){
+		$(this).toggleClass('open');
+	});
+});
 // Set the date we're counting down to
 var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
 
