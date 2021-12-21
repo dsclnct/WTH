8efc4e295d62fdbb80836646e226ab1ebe878915
weebo-ary <aryan.bhardwaj1233@gmail.com>
2021-12-22 03:41:48 +0530

    update

--- Code/js/main.js
+++ Code/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 	});
 });
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+var countDownDate = new Date("Jan 29, 2022 15:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
